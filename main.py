eb04a23ed796b44cd5dc9138a7b8f402de13ffd4
LewisDeZoete <lewis.dezoete@gmail.com>
2026-07-07 22:27:20 +1000

    change tiny cleanup of main.py

--- main.py
+++ main.py
@@ -2,13 +2,16 @@
 import argparse
 
 parser = argparse.ArgumentParser(
-    prog="MyDemo",
-    description="Print's the name passed as a command line argument."
+    prog="MyDemo", description="Print's the name passed as a command line argument."
 )
 
-parser.add_argument("-n", "--name", default="Lewis",
-                    help="The name to be printed out when running this script!")
+parser.add_argument(
+    "-n",
+    "--name",
+    default="Lewis",
+    help="The name to be printed out when running this script!",
+)
 
 args = parser.parse_args()
 
-print(f"Hello {args.name}")
+print(f"Hello, {args.name}!")
